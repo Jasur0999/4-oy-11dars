d0bb0bbfc596e1c7a38f88f98b781443a21f8a4c
Jasur0999 <leaderjasur@gmail.com>
2024-02-16 19:16:09 +0500

    fgnfnds

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+//  1 -task   ///////////////////
+
+// 1-masala
+// String.prototype.trm = function (){
+//     let trim;
+//    return this.trim()
+// }
+// const result = " salom js "
+// console.log(result.trm());
+
+//  2 -task   ///////////////////
+
+// String.prototype.upperCase = function(){
+//     let upper = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+//     let lower = 'abcdefghijklmnopqrstuvwxyz';
+//     let str = '';
+//         for(let i = 0; i < this.length; i++){
+//             let el = this[i]
+//             if(lower.includes(el)){
+//                 el = upper[lower.indexOf(el)];
+//             }
+//            str+=el;
+//         }
+//         return str;
+//     }
+//     let arr = 'john doe' ;
+//     let result = arr.upperCase();
+//     console.log(result);
+
+//  3 -task   ///////////////////
+
+// String.prototype.has =function(str){
+//     return this.indexOf(str) !== -1
+// }
+// let str1 = 'hello world';
+// let result = str1.has('hello')
+// console.log(result);
+
+//  4 -task   ///////////////////
+
+// String.prototype.cut = function (str, i){
+//     return this.substring(str,i)
+   
+// }
+// let str = 'hello world'
+// let s = str.cut(5,7)
+// console.log(s);
+
+
 //  11 - task  //////////////////
 
 // Array.prototype.customMap = function(tel) {
@@ -11,7 +60,6 @@
 // const all = arr.customMap(num => num * 2);
 // console.log(all);
 
-
 //   12 - task  ///////////////////
 
 // Array.prototype.customEvery = function(tel) {
@@ -24,4 +72,4 @@
 //   };
 // const arr = [1, 2, 3, 4, 5];
 // const result = arr.customEvery(num => num > 0);
-// console.log(result); 
+// console.log(result);
